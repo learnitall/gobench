8cb85814f6d5da2fec221433203436a7161b8bc6
Ryan Drew <learnitall0@gmail.com>
2022-02-08 18:41:52 -0700

    Adjust benchmarkable interface to allow for passing an exporter

--- define/benchmark.go
+++ define/benchmark.go
@@ -4,7 +4,15 @@ package define
 
 // Benchmarkable defines methods gobench needs to run a benchmark.
 type Benchmarkable interface {
+	// Setup runs tasks which need to be done prior to calling Run.
+	// These tasks could involve verifying integrity of config files or
+	// creating needed objects.
+	Setup(*Config) error
 	// Run actually runs the benchmark and sends the benchmark's result
 	// data to the Exporter. Assume benchmark passes if error is nil.
-	Run(*Config) error
+	Run(Exporterable) error
+
+	// Teardown does any finishing tasks after the benchmark has ran and
+	// exported its results.
+	Teardown(*Config) error
 }
