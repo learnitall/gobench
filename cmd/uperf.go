7b4334f2bb0130f4193b2481f3ad12745948b38b
Ryan Drew <learnitall0@gmail.com>
2022-02-08 18:41:36 -0700

    Define uperf command

--- cmd/uperf.go
+++ cmd/uperf.go
@@ -1,17 +1,30 @@
 package cmd
 
 import (
+	"github.com/learnitall/gobench/benchmarks/uperf"
 	"github.com/spf13/cobra"
 )
 
+func runUperf(cmd *cobra.Command, args []string) {
+	uperfCmdArgs := []string{
+		"uperf", "-m", args[0],
+	}
+	uperfCmdArgs = append(uperfCmdArgs, args[1:]...)
+
+	uperf := &uperf.UperfBenchmark{
+		WorkloadPath: args[0],
+		Cmd:          uperfCmdArgs,
+	}
+	RunBenchmark(uperf)
+}
+
 // uperfCmd represents the uperf command
 var uperfCmd = &cobra.Command{
-	Use:   "uperf workload",
+	Use:   "uperf workload options ...",
 	Short: "Run the uperf networking benchmark.",
-	Long:  `Uperf requires an xml file to define the workloads to run. This must be provided as the positional argument "workload".`,
-	Args:  cobra.ExactValidArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-	},
+	Long:  `Uperf requires an xml file to define the workloads to run. This must be provided as the positional argument "workload". If you would like to pass CLI arguments to uperf, place them after the workload filename.`,
+	Args:  cobra.MinimumNArgs(2),
+	Run:   runUperf,
 }
 
 func init() {
