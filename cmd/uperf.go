d0797cfd38cf58195bfa45bc2dfd955ebf6ae7b3
Ryan Drew <learnitall0@gmail.com>
2022-02-10 08:11:48 -0700

    Fix minimum args in uperf command

--- cmd/uperf.go
+++ cmd/uperf.go
@@ -1,3 +1,6 @@
+//go:build uperf
+// +build uperf
+
 package cmd
 
 import (
@@ -23,7 +26,7 @@ var uperfCmd = &cobra.Command{
 	Use:   "uperf workload options ...",
 	Short: "Run the uperf networking benchmark.",
 	Long:  `Uperf requires an xml file to define the workloads to run. This must be provided as the positional argument "workload". If you would like to pass CLI arguments to uperf, place them after the workload filename.`,
-	Args:  cobra.MinimumNArgs(2),
+	Args:  cobra.MinimumNArgs(1),
 	Run:   runUperf,
 }
 
